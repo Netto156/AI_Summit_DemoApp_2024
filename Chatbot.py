95323e63e3988687fbf6beec325ca5a1184d37d1
tom <tom.otten>
2024-05-10 17:42:13 +0200

    added styling

--- Chatbot.py
+++ Chatbot.py
@@ -4,6 +4,7 @@ import re
 import time
 from email.message import EmailMessage
 import smtplib
+from streamlit_extras.stylable_container import stylable_container
 # python -m streamlit run Chatbot.py
 
 # Set constants
@@ -11,6 +12,45 @@ MODEL_ID = st.secrets["model_id"]
 API_KEY = st.secrets["api_key"]
 EMAIL_PW = st.secrets["email_pw"]
 EMAIL_SENDER = st.secrets["email"]
+
+# -----------------------------------------------------------------------------
+
+MARKDOWN_CSS = """
+    div[data-testid="stMarkdown"] > p {
+        font-size: x-large;
+        font-family: sans-serif;
+        }
+"""
+
+MAIN_CSS = """
+    div[data-testid="stFullScreenFrame"] > div {
+        justify-content: center;
+        }
+    div[data-testid="stVerticalBlock"] > div[data-testid="element-container"] {
+        text-align: center;
+    }
+    div[data-testid="stChatMessage"] {
+        text-align: "left";
+    }
+"""
+
+FORM_CSS = """
+    div[data-testid="stMarkdown"] > div > p {
+        font-size: x-large;
+        color: #fe5000;
+        font-weight: bolder;
+    }
+
+    div[data-testid="stVerticalBlock"] {
+        text-align: center;
+    }
+
+    p {
+        font-family: sans-serif;
+        font-weight: bolder;
+    }
+"""
+
 
 #  ------------------------ Logic ---------------------------------------------
 @st.cache_resource
@@ -81,7 +121,6 @@ def query_assistant(user_input):
             thread_id=thread.id
         )
             GPT_response = messages.data[0].content[0].text.value
-            # print(GPT_response)
         else:
             print(run.status)  
     
@@ -89,7 +128,6 @@ def query_assistant(user_input):
 
 
 def mail_conversation():
-    print(st.session_state)
     if st.session_state["mail_my_result"] == "Yes":
         print("Sending conversation per mail...")
         email = st.session_state.email
@@ -106,21 +144,22 @@ def get_user_info():
     print("getting user info")
     st.session_state['Form_count'] += 1
     placeholder = st.empty()
-
     with placeholder.form(key="user_input_{0}".format(st.session_state['Form_count'])):
-        st.markdown("Please enter your contact details")
-        st.text_input("Company", placeholder="Enter the name of your company", key="Company")
-        st.text_input("First name", placeholder="Enter your first name", key="first_name")
-        st.text_input("Last name", placeholder="Enter your last name", key="last_name")
-        st.text_input("Email", placeholder="Enter your email", key="email")
-        col1, _, col3 = st.columns(3)
-        with col1:
-            submit_button = st.form_submit_button("Submit", on_click=validate_user_input)
-        with col3:
-            st.link_button("See our privacy policy", "https://www.incentro.com/nl-NL/privacy-policy")
+        with stylable_container(key="Start_dialog", css_styles=FORM_CSS):
 
-        if submit_button:
-            placeholder.empty()
+            st.markdown("Please enter your contact details to continue")
+            st.text_input("Company", placeholder="Enter the name of your company", key="Company")
+            st.text_input("First name", placeholder="Enter your first name", key="first_name")
+            st.text_input("Last name", placeholder="Enter your last name", key="last_name")
+            st.text_input("Email", placeholder="Enter your email", key="email")
+            col1, _, col3 = st.columns(3)
+            with col1:
+                submit_button = st.form_submit_button("Submit", on_click=validate_user_input)
+            with col3:
+                st.link_button("See our privacy policy", "https://www.incentro.com/nl-NL/privacy-policy")
+
+            if submit_button:
+                placeholder.empty()
     return
 
 
@@ -145,28 +184,30 @@ def send_email(recipient, message):
 
 
 def end_the_conversation():
-    placeholder = st.empty()
-    with placeholder.form("Send_email"):
-        st.markdown("Result of the conversation")
-        col1, col2 = st.columns(2)
-        with col1:
-            st.radio("Did you like this tool?", ["Yes", "No"], key="liked")
-            print(st.session_state)
+    print("ending conv..")
+    # placeholder_for_radio1 = st.radio("Did you like this tool?", ["Yes", "No"], key="liked", index=None)
+    # placeholder_for_radio2 = st.radio("Would you like to receive a copy of the conversation per email?", ["Yes", "No"], horizontal=True, key="mail_my_result", index=None)
+    st.markdown("Result of the conversation")
+    # col1, col2 = st.columns(2)
+    # with col1:
+    placeholder_for_radio1 = st.radio("Did you like this tool?", ["Yes", "No"], key="liked", index=None)
 
-        with col2:
-            # st.radio("Would you like to get in contact with Incentro", ["Yes", "Maybe", "No"], key="contact_me")
-            st.radio("Would you like to receive a copy of the conversation per email?", ["Yes", "No"], horizontal=True, key="mail_my_result")
-
-        submit_button = st.form_submit_button("Submit")
-        if submit_button:
-            placeholder.empty()
+    # with col2:
+    placeholder_for_radio2 = st.radio("Would you like to receive a copy of the conversation per email?", ["Yes", "No"], horizontal=True, key="mail_my_result", index=None)
+    
     print(st.session_state)
-    st.session_state['Conversation_ended'] = True
+    if st.button("Submit"):
+        # placeholder.empty()
+        st.session_state['Conversation_ended'] = True
+        st.session_state['Ending_conversation'] = False
+        st.rerun()
 
 
 def conversation_ended():
-    st.markdown("Thank you for your time, we hope you found this usefull")
-    st.markdown("Want to see other succes stories? See ... ")
+    with stylable_container(key="Final_dialog", css_styles=MARKDOWN_CSS):
+        st.markdown("Thank you for your time, we hope you found this usefull")
+        st.link_button("See other succes stories", "https://www.incentro.com/en")
+    
     # st.markdown("Thank you for your time, we hope you found this usefull")
 
 
@@ -175,9 +216,10 @@ for x,y in st.session_state.items():
     st.session_state[x] = y
 
 # ---------------------------------- UI Main page --------------------------------------------
-
-st.title("Hyper Automation Discoverer")
-st.caption("🚀 a chatbot to help you discovery you automation potential")
+with stylable_container(key="main_page", css_styles=MAIN_CSS):
+    st.image("https://0097f9ca.flyingcdn.com/wp-content/uploads/2022/04/Incentro-logo-2018-Orange-1024x174.png", width=200)
+    st.title("Hyper Automation Discoverer")
+    st.caption("🤖 A chatbot to help you discovery you automation potential 🚀")
 
 # Initialize variables
 if "Valid_input" not in st.session_state:
@@ -185,19 +227,26 @@ if "Valid_input" not in st.session_state:
     st.session_state['Valid_input'] = False
     st.session_state['Conversation_ended'] = False
     st.session_state['Form_count'] = 0
+    st.session_state['Ending_conversation'] = False
+
     create_new_thread()
+
 
 # Get user input before starting the chat
 if st.session_state['Valid_input'] == False:
     if st.session_state['Form_count'] > 0:
         get_user_info()
-        st.info("Please correctly fill in this form first")
+        st.info("To continue, please correctly fill in this form")
     else:
         get_user_info()
 
+# get user input at the end of the conversation
+if st.session_state['Ending_conversation']:
+    end_the_conversation()
+
 
 # Start chat with user
-if st.session_state['Valid_input'] and not st.session_state['Conversation_ended']:
+if st.session_state['Valid_input'] and not st.session_state['Conversation_ended'] and not st.session_state['Ending_conversation']:
     if "messages" not in st.session_state:
         st.session_state["messages"] = [{"role": "assistant", "content": "Hi {0}, I'd like to help you discovery your automation potential. In what branch do you work?".format(st.session_state.first_name)}]
 
@@ -219,9 +268,10 @@ if st.session_state['Valid_input'] and not st.session_state['Conversation_ended'
 
     # End conversation
     if st.button("End conversation"):
-        end_the_conversation()
-        mail_conversation()
-        st.stop()
+        st.session_state["Ending_conversation"] = True
+        st.rerun()
 
+# Conversation has ended and last pop-up was filled in
 if st.session_state['Conversation_ended']:
+    mail_conversation()
     conversation_ended()
